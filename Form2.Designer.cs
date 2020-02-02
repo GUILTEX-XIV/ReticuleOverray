33feb06bb2a7ac79fead8b92f06dc1f140596684
GUILTEX <guiltydrk@gmail.com>
2020-02-02 20:20:37 +0900

    照準が画面中央ではないゲームがあるため苦戦中
    とりあえず、1920×1080用ということで更新

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.SuspendLayout();
             // 
             // shapeContainer1
@@ -45,16 +45,6 @@
             this.shapeContainer1.TabIndex = 0;
             this.shapeContainer1.TabStop = false;
             // 
-            // lineShape1
-            // 
-            this.lineShape1.BorderColor = System.Drawing.Color.Red;
-            this.lineShape1.BorderWidth = 2;
-            this.lineShape1.Name = "lineShape1";
-            this.lineShape1.X1 = 15;
-            this.lineShape1.X2 = 45;
-            this.lineShape1.Y1 = 30;
-            this.lineShape1.Y2 = 30;
-            // 
             // lineShape2
             // 
             this.lineShape2.BorderColor = System.Drawing.Color.Red;
@@ -65,6 +55,16 @@
             this.lineShape2.Y1 = 15;
             this.lineShape2.Y2 = 45;
             // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.Color.Red;
+            this.lineShape1.BorderWidth = 2;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 15;
+            this.lineShape1.X2 = 45;
+            this.lineShape1.Y1 = 30;
+            this.lineShape1.Y2 = 30;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -72,9 +72,9 @@
             this.ClientSize = new System.Drawing.Size(60, 60);
             this.Controls.Add(this.shapeContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Location = new System.Drawing.Point(932, 512);
+            this.Location = new System.Drawing.Point(930, 510);
             this.Name = "Form2";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form2";
             this.TopMost = true;
             this.TransparencyKey = System.Drawing.SystemColors.Control;
